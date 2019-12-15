a2fc5e650bd957461736dde06af703f1da0549bf
Patrick Kostjens <mail@patrickkostjens.nl>
2019-12-15 11:01:49 +0100

    Cleanup permissions, change crash report settings after fork

--- app/src/main/java/nl/patrickkostjens/kandroid/KandroidApplication.java
+++ app/src/main/java/nl/patrickkostjens/kandroid/KandroidApplication.java
@@ -27,19 +27,17 @@ import org.acra.*;
 import org.acra.annotation.*;
 import org.acra.sender.HttpSender;
 
-@ReportsCrashes(//mailTo = "kandroid@andres.in",
-                formUri = "https://uber.andres.in/crashreport/report.php",
-                //customReportContent = { ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.USER_COMMENT, ReportField.STACK_TRACE, ReportField.LOGCAT },
+@ReportsCrashes(formUri = "https://crash.patrickkostjens.nl/report",
                 mode = ReportingInteractionMode.DIALOG,
+                formUriBasicAuthLogin = "UGosGwubLYytIPAM",
+                formUriBasicAuthPassword = "nDOMOkZd6zbaHNuI",
                 excludeMatchingSharedPreferencesKeys={"username", "password", "serverurl"},
-                httpMethod = HttpSender.Method.PUT,
+                httpMethod = HttpSender.Method.POST,
                 reportType = HttpSender.Type.JSON,
                 logcatArguments = { "-t", "200", "-v", "time", "Kandroid:d", "InstantRun:s", "*:e" },
                 resDialogIcon = android.R.drawable.stat_notify_error,
                 resDialogTitle = R.string.acra_dialog_title,
-//                resDialogText = R.string.acra_dialog_text,
                 resDialogText = R.string.acra_dialog_text_upload,
-//                resDialogPositiveButtonText = R.string.acra_send_mail,
                 resDialogCommentPrompt = R.string.acra_dialog_comment_prompt)
 public class KandroidApplication extends Application {
     @Override
